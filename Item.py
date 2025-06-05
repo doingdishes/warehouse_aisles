b6f54d5a310c1203cd96c3a1dd8667143b496926
Chris Clair <chrisclair@Chriss-MacBook-Pro-3.local>
2025-06-05 13:33:54 -0400

    worked on item class methods

--- Item.py
+++ Item.py
@@ -1,23 +1,42 @@
 #defines the item class. 
 #items are stored in aisles
+#need to work on CRUD commands
 
 class Item:
 
-    def __init__(self, category, ORMD,):
+    def __init__(self, name, count, ORMD, sku):
         """
-        Constructor to initialize instance attributes.
+        the item class defines the stockable goods
+        in each aisle object
+        goal is to be able to perform
+        CRUD operations on items
         """
-        self.category = category
+        self.name = str(name) #name of the item
+        self.count = int(count) #quantity/count of the item
+        self.sku = str(sku) #Stock Keeping Unit number
         self.ORMD = True
 
-    def create_item(self):
+    def create_item(self, name, count, sku):
         """
-        Method performing an action using instance attributes.
+        Method that creates an item object
         """
-        return f"Method 1: {self.instance_attribute1}"
+        item_name = str(input('Item Name: '))
+        item_count = int(input('Item Count (as an integer): '))
+        item_sku = str(input('Item SKU (as an integer): '))
+        return item_name, item_count, item_sku
 
-    def method2(self, parameter):
+    def read_item(self, name, count, sku):
          """
-         Method performing an action using instance attributes and parameters.
+         Method that reports the name and count of the item
          """
-         return f"Method 2: {self.instance_attribute2}, {parameter}"
+    
+    def update_item(self, name, count, sku):
+        """
+        method that updates either the name or count of the item
+        """
+        return f""
+
+    def delete_item(self, name, count, sku):
+        """
+        removes an item from the database
+        """
